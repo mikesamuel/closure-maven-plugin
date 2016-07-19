26c8d2ea6c1491821808fa5a1ab33cbf69c18a26
Mike Samuel <mikesamuel@gmail.com>
2016-07-19 13:56:44 -0400

    use accessible stylesheet compiler ctor instead of subclassing

--- src/main/java/com/google/common/html/plugin/css/CssCompilerWrapper.java
+++ src/main/java/com/google/common/html/plugin/css/CssCompilerWrapper.java
@@ -3,8 +3,6 @@ package com.google.common.html.plugin.css;
 import java.io.File;
 import java.io.IOException;
 
-import javax.annotation.Nullable;
-
 import org.apache.maven.plugin.logging.Log;
 
 import com.google.common.base.Charsets;
@@ -79,22 +77,8 @@ final class CssCompilerWrapper {
 
     ErrorManager errorManager = new MavenCssErrorManager(log);
 
-    final class Compiler extends ClosureCommandLineCompiler {
-      Compiler(
-          JobDescription job,
-          ExitCodeHandler exitCodeHandler,
-          ErrorManager errManager) {
-        super(job, exitCodeHandler, errManager);
-      }
-
-      @Override
-      public String execute(
-          @Nullable File renameFilePath, @Nullable File sourceMapFilePath) {
-        return super.execute(renameFilePath, sourceMapFilePath);
-      }
-    }
-
-    String compiledCss = new Compiler(job, exitCodeHandler, errorManager)
+    String compiledCss =
+        new ClosureCommandLineCompiler(job, exitCodeHandler, errorManager)
         .execute(renameFile.orNull(), sourceMapFile.orNull());
     if (compiledCss == null) {
       return false;
