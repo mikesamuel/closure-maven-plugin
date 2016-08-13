a5a9d0ec80488f9df6e17415b2171827c3c190d5
Mike Samuel <mikesamuel@gmail.com>
2016-08-13 01:02:54 -0400

    added a JUnit test to the simple integration test which fires up the compiled JS in a Rhino interpreter

--- src/it/simple-it/src/main/js/hello_world.js
+++ src/it/simple-it/src/main/js/hello_world.js
@@ -1,16 +1,18 @@
 goog.module('hello.world');
 
-goog.require('proto.com.example.Name');
+goog.require('goog.html.SafeHtml');
 goog.require('greetings.world');
+goog.require('proto.com.example.Name');
 
 /** @type {proto.com.example.Name} */
 var myWorld = new proto.com.example.Name();
 myWorld.setText('Cle<eland');
 
+/** @type {goog.html.SafeHtml} */
 var myGreeting = greetings.world.HelloWorld(
   {
     world: myWorld
   }
 );
 
-alert(myGreeting);
+alert(myGreeting.getContent());
---
+++ src/it/simple-it/src/test/java/com/example/CompiledJsTest.java
@@ -0,0 +1,58 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.NativeArray;
+import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.ScriptableObject;
+
+import com.google.common.base.Charsets;
+
+import junit.framework.TestCase;
+
+@SuppressWarnings("javadoc")
+public final class CompiledJsTest extends TestCase {
+
+  private Context cx;
+  private Scriptable scope;
+
+  @Before
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    cx = Context.enter();
+    scope = cx.initSafeStandardObjects();
+    // Interpret since compiled JS can blow out the method bytecode limit
+    // when compiling to bytecode.
+    cx.setOptimizationLevel(-1);
+  }
+
+  private void loadJsModule(String moduleResourcePath) throws IOException {
+    try (InputStream in = getClass().getResourceAsStream(moduleResourcePath)) {
+      try (Reader code = new InputStreamReader(in, Charsets.UTF_8)) {
+        cx.evaluateReader(scope, code, moduleResourcePath, 1, null);
+      }
+    }
+  }
+
+  @Test
+  public void testCompiledJs() throws Exception {
+    cx.evaluateString(
+        scope, "var _alerts_ = []; alert = function (s) { _alerts_.push(s); }",
+        "test", 1, null);
+
+    loadJsModule("/js/main.js");
+    loadJsModule("/js/hello.world.js");
+
+    NativeArray alerts = (NativeArray)
+        ScriptableObject.getProperty(scope, "_alerts_");
+
+    assertEquals("Hello, <b>Cle&lt;eland</b>!", cx.toString(alerts.get(0)));
+  }
+}
