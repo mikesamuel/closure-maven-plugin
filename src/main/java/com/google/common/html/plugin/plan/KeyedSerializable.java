379ac9e782990d91c39cd6637789e7b18ea66142
Mike Samuel <mikesamuel@gmail.com>
2016-08-16 12:33:37 -0400

    doced

--- src/main/java/com/google/common/html/plugin/plan/KeyedSerializable.java
+++ src/main/java/com/google/common/html/plugin/plan/KeyedSerializable.java
@@ -2,6 +2,15 @@ package com.google.common.html.plugin.plan;
 
 import java.io.Serializable;
 
+/**
+ * A serializable can be hashed, and the key can be used to persist
+ * derived information along with the hash allowing the hash to be used to
+ * test the freshness of the derived information.
+ */
 public interface KeyedSerializable extends Serializable {
+  /**
+   * A key that is unique among a set of items that might appear in a hash
+   * store together.
+   */
   PlanKey getKey();
 }
