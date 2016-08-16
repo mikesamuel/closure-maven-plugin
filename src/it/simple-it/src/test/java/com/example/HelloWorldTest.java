1b432014efdb11d2cf300332847671b714da40cb
Mike Samuel <mikesamuel@gmail.com>
2016-08-16 12:20:41 -0400

    remove dependencies on closure-templates fork by using the PrecompiledSoyModule to provide jbcsrc backend support.  PrecompiledSoyModule is in the process of being open-sourced so is still only available via the fork

--- src/it/simple-it/src/test/java/com/example/HelloWorldTest.java
+++ src/it/simple-it/src/test/java/com/example/HelloWorldTest.java
@@ -1,9 +1,8 @@
-package com.google.common.html.plugin;
+package com.example;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
 import java.io.IOException;
 import java.io.File;
 import java.util.List;
@@ -13,30 +12,26 @@ import java.util.Map;
 import com.example.Proto1.Name;
 
 // Soy uses Guice to inject stuff.  Sigh.
+import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
 
 // Template-support
 import com.google.common.html.types.SafeHtmls;
 import com.google.protobuf.Descriptors.DescriptorValidationException;
-import com.google.template.soy.coredirectives.EscapeHtmlDirective;
 import com.google.template.soy.data.SanitizedContent;
+import com.google.template.soy.data.SanitizedContent.ContentKind;
 import com.google.template.soy.data.SoyCustomValueConverter;
-import com.google.template.soy.data.SoyEasyDict;
 import com.google.template.soy.data.SoyValueHelper;
-import com.google.template.soy.jbcsrc.api.AdvisingStringBuilder;
 import com.google.template.soy.jbcsrc.api.RenderResult;
-import com.google.template.soy.jbcsrc.shared.CompiledTemplates;
-import com.google.template.soy.jbcsrc.shared.RenderContext;
-import com.google.template.soy.shared.restricted.SoyJavaPrintDirective;
-import com.google.template.soy.types.SoyTypeProvider;
-import com.google.template.soy.types.SoyTypeRegistry;
+import com.google.template.soy.jbcsrc.api.Precompiled;
+import com.google.template.soy.jbcsrc.api.PrecompiledSoyModule;
+import com.google.template.soy.jbcsrc.api.SoySauce;
 import com.google.template.soy.types.proto.SoyProtoTypeProvider;
 import com.google.template.soy.types.proto.SoyProtoValueConverter;
-
-// Compiled templates
-import com.google.template.soy.jbcsrc.gen.greetings.world.HelloWorld;
-
 
 // Testbed imports
 import org.junit.Test;
@@ -45,80 +40,58 @@ import junit.framework.TestCase;
 @SuppressWarnings("javadoc")
 public final class HelloWorldTest extends TestCase {
 
-  final CompiledTemplates compiledTemplates = new CompiledTemplates(
-      ImmutableSet.<String>of());
+  static final String TEMPLATE_NAME = "greetings.world.HelloWorld";
+  private final Injector injector;
+
+  @Inject
+  SoyValueHelper valueHelper;
+
+  @Inject
+  @Precompiled
+  SoySauce soySauce;
+
   {
-    compiledTemplates.loadAll(ImmutableList.of("greetings.world.HelloWorld"));
+    injector = Guice.createInjector(new TestModule());
+    injector.injectMembers(this);
   }
 
-  // TODO: These should be populable by CoreDirectivesModule or
-  // PrecompiledSoyModule.
-  final SoyProtoTypeProvider protoTypeProvider;
 
-  {
-    File descriptorFile = new File(
-        Joiner.on(File.separator).join(
-            "target", "src", "main", "proto", "descriptors.pd"));
-    try {
-      protoTypeProvider = new SoyProtoTypeProvider.Builder()
-          .addFileDescriptorSetFromFile(descriptorFile)
-          .build();
-    } catch (IOException ex) {
-      throw (AssertionError) new AssertionError().initCause(ex);
-    } catch (DescriptorValidationException ex) {
-      throw (AssertionError) new AssertionError().initCause(ex);
+  SanitizedContent renderHelloWorld(
+      Map<String, ?> data, Map<String, ?> ijData) {
+    SoySauce.Renderer renderer = soySauce
+            .renderTemplate(TEMPLATE_NAME)
+            .setData(data)
+            .setIj(ijData)
+//          .setMsgBundle(msgBundle)
+//          .setXidRenamingMap(idRenamingMap)
+//          .setCssRenamingMap(cssRenamingMap)
+            ;
+
+    SoySauce.Continuation<SanitizedContent> c = renderer.renderStrict();
+    for (int tries = 100; --tries >= 0;) {
+      RenderResult result = c.result();
+      if (result.isDone()) {
+        return c.get();
+      }
+      c = c.continueRender();
     }
-  }
-
-  final SoyTypeRegistry typeRegistry = new SoyTypeRegistry(
-      ImmutableSet.<SoyTypeProvider>of(protoTypeProvider));
-  
-  final ImmutableMap<String, SoyJavaPrintDirective> printDirectiveMap =
-      ImmutableMap.<String, SoyJavaPrintDirective>of(
-          EscapeHtmlDirective.NAME, new EscapeHtmlDirective());
-
-  final ImmutableList<SoyCustomValueConverter> customConverters =
-      ImmutableList.<SoyCustomValueConverter>of(
-          new SoyProtoValueConverter(typeRegistry, protoTypeProvider));
-
-  // TODO: similarly injectable
-  final SoyValueHelper valueHelper = new SoyValueHelper(customConverters);
-
-  String renderHelloWorld(Map<String, ?> data, Map<String, ?> ijData)
-  throws IOException {
-    RenderContext rc = new RenderContext.Builder()
-        .withCompiledTemplates(compiledTemplates)
-        .withSoyPrintDirectives(printDirectiveMap)
-        .withConverter(valueHelper)
-        .build();
-
-    SoyEasyDict dataDict = valueHelper.newEasyDict();
-    dataDict.setFieldsFromJavaStringMap(data);
-    dataDict.makeImmutable();
-
-    SoyEasyDict ijDataDict = valueHelper.newEasyDict();
-    ijDataDict.setFieldsFromJavaStringMap(ijData);
-    ijDataDict.makeImmutable();
-
-    HelloWorld hw = new HelloWorld(dataDict, ijDataDict);
-    AdvisingStringBuilder out = new AdvisingStringBuilder();
-    RenderResult result = hw.render(out, rc);
-    assertTrue(result.isDone());
-    return out.toString();
+    throw new IllegalStateException(
+        "Rendering stuck on " + c.result().future());
   }
 
   @Test
-  public final void testHelloWorldNoInput() throws Exception {
+  public final void testHelloWorldNoInput() {
     ImmutableMap<String, Object> data = ImmutableMap.of();
     ImmutableMap<String, Object> ijData = ImmutableMap.of();
 
-    String output = renderHelloWorld(data, ijData);
-    assertEquals("Hello, <b>World</b>!", output);
+    SanitizedContent output = renderHelloWorld(data, ijData);
+    assertEquals(ContentKind.HTML, output.getContentKind());
+    assertEquals("Hello, <b>World</b>!", output.getContent());
   }
 
 
   @Test
-  public final void testHelloWorldUntrustedTextInput() throws Exception {
+  public final void testHelloWorldUntrustedTextInput() {
     Name.Builder nameBuilder = Name.newBuilder();
     nameBuilder.setText("Cincinatti <:)>");
 
@@ -126,12 +99,13 @@ public final class HelloWorldTest extends TestCase {
         "world", nameBuilder.build());
     ImmutableMap<String, Object> ijData = ImmutableMap.of();
 
-    String output = renderHelloWorld(data, ijData);
-    assertEquals("Hello, <b>Cincinatti &lt;:)&gt;</b>!", output);
+    SanitizedContent output = renderHelloWorld(data, ijData);
+    assertEquals(ContentKind.HTML, output.getContentKind());
+    assertEquals("Hello, <b>Cincinatti &lt;:)&gt;</b>!", output.getContent());
   }
 
   @Test
-  public final void testHelloWorldSafeHtmlInput() throws Exception {
+  public final void testHelloWorldSafeHtmlInput() {
     Name.Builder nameBuilder = Name.newBuilder();
     nameBuilder.setHtml(SafeHtmls.toProto(SafeHtmls.htmlEscape(
         "Cincinatti <:-}>")));
@@ -140,7 +114,41 @@ public final class HelloWorldTest extends TestCase {
         "world", nameBuilder.build());
     ImmutableMap<String, Object> ijData = ImmutableMap.of();
 
-    String output = renderHelloWorld(data, ijData);
-    assertEquals("Hello, <b>Cincinatti &lt;:-}&gt;</b>!", output);
+    SanitizedContent output = renderHelloWorld(data, ijData);
+    assertEquals(ContentKind.HTML, output.getContentKind());
+    assertEquals("Hello, <b>Cincinatti &lt;:-}&gt;</b>!", output.getContent());
+  }
+
+  @SuppressWarnings("static-method")
+  public static final class TestModule extends AbstractModule {
+    @Override
+    protected void configure() {
+      // This installs all the core plugins and the apicallscope dependencies.
+      install(new PrecompiledSoyModule());
+    }
+
+    @Provides
+    @Singleton
+    public List<SoyCustomValueConverter> provideSoyValueConverters(
+        SoyProtoValueConverter protoConverter) {
+      System.err.println("PROVIDE VALUE CONVERTERS");
+      // Note: The order of data converters matters. Converters that only accept
+      // specific input types should come before converters that will convert
+      // anything.
+      return ImmutableList.<SoyCustomValueConverter>of(protoConverter);
+    }
+
+    @Provides
+    @Singleton
+    public SoyProtoTypeProvider provideProtoTypeProvider()
+    throws IOException, DescriptorValidationException {
+      System.err.println("PROVIDE PROTO TYPE PROVIDER");
+      File descriptorFile = new File(
+          Joiner.on(File.separator).join(
+              "target", "src", "main", "proto", "descriptors.pd"));
+      return (new SoyProtoTypeProvider.Builder())
+          .addFileDescriptorSetFromFile(descriptorFile)
+          .build();
+    }
   }
 }
