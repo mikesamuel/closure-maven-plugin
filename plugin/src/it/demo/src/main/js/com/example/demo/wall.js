2b23ec6fb6a115563b5b9b3f9f4677d60c10f30f
Mike Samuel <mikesamuel@gmail.com>
2016-08-23 15:44:02 -0400

    fleshed out demo

--- plugin/src/it/demo/src/main/js/com/example/demo/wall.js
+++ plugin/src/it/demo/src/main/js/com/example/demo/wall.js
@@ -1,3 +1,96 @@
 goog.module('com.example.wall');
 
-// TODO
+goog.require('goog.events');
+goog.require('goog.events.EventType');
+goog.require('goog.fx.DragDrop');
+goog.require('goog.style');
+
+goog.events.listen(
+  document,
+  goog.events.EventType.DOMCONTENTLOADED,
+  function () {
+    var scribbleForm = document.querySelector('form#scribble');
+    var submitButton = scribbleForm.querySelector('button#scribble-submit');
+    var htmlTextarea = scribbleForm.querySelector('textarea#html');
+    var xPosition = scribbleForm.querySelector('input#x');
+    var yPosition = scribbleForm.querySelector('input#y');
+
+    /**
+     * A wall-item under development that has not been committed to the
+     * backend.
+     *
+     * @param {boolean} opt_forceVisible
+     * @return {HTMLElement}
+     */
+    function getEditorChip(opt_forceVisible) {
+      var chip = document.getElementById('chip');
+      if (opt_forceVisible) {
+        // The chip is invisible by default.
+        goog.style.setStyle(chip, 'display', 'inline-block');
+      }
+      return chip;
+    }
+
+    goog.events.listen(
+      htmlTextarea, goog.events.EventType.CHANGE,
+      rateLimit(function () {
+        goog.dom.setInnerHtml(
+          getEditorChip(),
+          htmlTextarea.value);
+      }));
+
+    goog.events.listen(
+      xPosition, goog.events.EventType.CHANGE,
+      rateLimit(function () {
+        var pct = Math.max(0, Math.min(100, +xPosition.value));
+        if (pct === pct) {
+          goog.style.setStyle(getEditorChip(), 'left', pct + '%');
+        }
+      }));
+
+    goog.events.listen(
+      yPosition, goog.events.EventType.CHANGE,
+      rateLimit(function () {
+        var pct = Math.max(0, Math.min(100, +yPosition.value));
+        if (pct === pct) {
+          goog.style.setStyle(getEditorChip(), 'top', pct + '%');
+        }
+      }));
+
+    goog.events.listen(
+      submitButton, goog.events.EventType.CLICK,
+      function () {
+        console.log('clicked');
+      });
+
+
+    /**
+     * A function that, when called, causes f to be called
+     * within roughly opt_rate_ms, but all other calls within
+     * that window will be folded into one call to f.
+     *
+     * @param {!function ():void} f
+     * @param {number} opt_rate_ms
+     * @return {!function ():void}
+     */
+    function rateLimit(f, opt_rate_ms) {
+      var rate_ms = opt_rate_ms || 100 /* ms */;
+      /** {@type number|null} */
+      var timeoutId = null;
+
+      if (!(rate_ms > 0)) {
+        throw new Error(rate_ms);
+      }
+
+      return function () {
+        if (timeoutId === null) {
+          timeoutId = setTimeout(
+            function () {
+              timeoutId = null;
+              f();
+            },
+            rate_ms);
+        }
+      };
+    }
+  });
--- run_demo_server.sh
+++ run_demo_server.sh
@@ -6,25 +6,38 @@ cd -- "$(dirname "$0")"
 
 
 # separate flags meant for mvn build from those meant for the demo server
-MVN_FLAGS=""
+MVN_PLUGIN_FLAGS=""
+MVN_DEMO_FLAGS=""
 
-case "$1" in
-    "clean" | "-X")
-        MVN_FLAGS="$MVN_FLAGS $1"
-        shift
-        ;;
-esac
+while true; do
+    case "$1" in
+        "fast")
+            MVN_PLUGIN_FLAGS="$MVN_PLUGIN_FLAGS -DskipTests=true"
+            shift
+            ;;
+        "clean" | "-X")
+            MVN_PLUGIN_FLAGS="$MVN_PLUGIN_FLAGS $1"
+            MVN_DEMO_FLAGS="$MVN_DEMO_FLAGS $1"
+            shift
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
 
 
 # pick a demo target directory outside the main project tree
 export DEMO_TARGET_DIR="$TMPDIR/closure-demo-target"
 
 # build the plugin
-mvn install
+mvn install -Djava.awt.headless=true $MVN_PLUGIN_FLAGS
 
 # build the demo
 pushd "$PWD/plugin/src/it/demo/"
-mvn -PalternateBuildDir -Dalt.build.dir="$DEMO_TARGET_DIR" $MVN_FLAGS verify
+mvn -PalternateBuildDir -Dalt.build.dir="$DEMO_TARGET_DIR" \
+    -Djava.awt.headless=true \
+    $MVN_DEMO_FLAGS verify
 popd
 
 # execute the jar
